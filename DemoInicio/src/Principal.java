aa504d98986abf67d927fe8f842d2df03a138403
Miguel Rugerio <mrugerio@gmail.com>
2022-07-12 11:16:03 -0500

    String StringBuilder

--- DemoInicio/src/Principal.java
+++ DemoInicio/src/Principal.java
@@ -2,7 +2,38 @@
 public class Principal {
 
 	public static void main(String[] args) {
-		System.out.println("Hola Mundo");
+		
+		String s = new String("Hola"); //Objeto	Inmutable	
+		s.concat(" Mundo");
+		System.out.println(s); //Hola
+		
+		StringBuilder sb = new StringBuilder("Hello"); //Objeto
+		sb.append(" World");
+		System.out.println(sb); //Hello World
+		
+		sb = null;
+		
+		int i = 10; //Primitivo
+		i = i + 5;
+		System.out.println(i); //5
+		
+		
+		String s1 = "Otro Hola";
+		String s2 = "Otro Hola"; //1
+		String s3 = new String("Otro Hola"); //2
+		
+		System.out.println(s1==s2); //true
+		System.out.println(s1==s3); //false
+		System.out.println("*************");
+		
+		String nombre = "Patrobas";
+		
+		for (int x = 0; x<100; x++ ) {
+			nombre = nombre + x;
+			System.out.println(nombre);
+		}
+		
+		
 	}
 
 }
